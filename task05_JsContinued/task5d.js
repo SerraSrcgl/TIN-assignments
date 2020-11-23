973fdc852875239ccc662710f93205551127bc72
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2020-11-23 12:01:24 +0100

    Create task5d.js

---
+++ task05_JsContinued/task5d.js
@@ -0,0 +1,29 @@
+// Getter and Setter in object initializer
+let student = {
+    firstName: 'Serra',
+    lastName: 'Saracoglu',
+    id: 1,
+    courses: ['Math', 'English', 'Informatics'],
+    grades: [1, 2, 3, 4, 5],
+    
+
+ get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+
+    set fullName(name) {
+    [this.firstName, this.lastName] = value.split(" ");
+},
+
+// set fullName is executed with the given value.user.fullName = "Sheldon Cooper";
+    get averageGrade() {
+        if (this.grades.length === 0)
+            return 0;
+        return this.grades.reduce((sum, grade) => sum + grade) / this.grades.length;
+    }
+}
+
+
+console.log("full name:", student.fullName, "\ngrades average:", student.averageGrade);
+student.fullName = "Serra Saracoglu";
+console.log("new full name:", student.fullName);
